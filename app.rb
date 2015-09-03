2fdd8b1c6eb7f277d6252d0fc0d0d58680e625ae
Amit <amitshob@gmail.com>
2015-09-03 10:58:20 -0700

    first commit

--- app.rb
+++ app.rb
@@ -16,10 +16,6 @@ post('/new_word') do
 	redirect('/')
 end
 
-post('/clear_words') do
-	Word.clear()
-	redirect('/')
-end
 
 get('/word/:id') do
 	@word = Word.find(params.fetch('id').to_i())
@@ -33,22 +29,5 @@ post('/add_definition') do
 	new_definition = Definition.new(type, definition)
 	@word = Word.find(id.to_i())
 	@word.add_definition(new_definition)
-	redirect('/word/' + @word.id().to_s())
-end
-
-post('/clear_definitions') do
-	id = params.fetch('word_id')
-	@word = Word.find(id.to_i())
-	@word.clear_definitions()
-	redirect('/word/' + @word.id().to_s())
-end
-
-post('/add_example') do
-	example = params.fetch('sentence')
-	word_id = params.fetch('word_id')
-	definition = params.fetch('definition')
-	@word = Word.find(word_id.to_i())
-	@definition = @word.find_definition(definition)
-	@definition.add_example(example)
 	redirect('/word/' + @word.id().to_s())
 end
